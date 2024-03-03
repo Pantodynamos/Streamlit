4d98956b59d589450d37e8cdfd640ec88c909cb8
Pantodynamos <96879052+Pantodynamos@users.noreply.github.com>
2024-03-03 10:57:20 +0700

    Update streamlit_app.py for date slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+import seaborn as sns
 
 st.title('Dashboard for Shared Bike Riding Data Analysis')
 
@@ -9,3 +11,16 @@ uploaded_file = st.file_uploader('Choose a CSV file')
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.dataframe(df)
+
+bike_dataframe_day = pd.read_csv("day.csv")
+
+min_date = bike_dataframe_day["dteday"].min()
+max_date = bike_dataframe_day["dteday"].max()
+
+with st.sidebar:
+
+  start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
